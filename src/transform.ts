02fc96a7d59123a5c0361e24a2009c78f94382db
Alan Pierce <alangpierce@gmail.com>
2017-09-29 16:11:36 -0500

    Some initial code based on a babylon fork and some failing tests

--- src/transform.ts
+++ src/transform.ts
@@ -1,3 +1,37 @@
-export default function transform(s: string): string {
-  return s + ';';
+import {tokenize} from 'babylon';
+
+export default function transform(code: string): string {
+  let result = '';
+  const tokens = tokenize(code, {sourceType: 'module', plugins: ['jsx']});
+
+  for (let i = 0; i < tokens.length; i++) {
+    const token = tokens[i];
+    result += code.slice(i > 0 ? tokens[i - 1].end : 0, token.start);
+    if (token.type.label === 'jsxTagStart' && tokens[i + 1].type.label === '/') {
+      // Closing tag.
+      result += ')';
+      while (tokens[i].type.label !== 'jsxTagEnd') {
+        i++;
+      }
+    } else if (token.type.label === 'jsxTagStart') {
+      result += 'React.createElement(';
+      if (tokens[i + 1].type.label === 'jsxName' && startsWithLowerCase(tokens[i + 1].value)) {
+        i++;
+        result += `'${tokens[i].value}'`;
+      }
+    } else if (token.type.label === 'jsxTagEnd') {
+      result += ', ';
+    } else if (token.type.label === '/' && tokens[i + 1].type.label === 'jsxTagEnd') {
+      result += ')';
+      i++;
+    } else {
+      result += code.slice(token.start, token.end);
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+function startsWithLowerCase(s: string): boolean {
+  return s[0] == s[0].toLowerCase();
 }
--- test/transform-test.ts
+++ test/transform-test.ts
@@ -3,7 +3,19 @@ import * as assert from 'assert';
 import transform from '../src/transform';
 
 describe('transform', () => {
-  it('does a simple tranform', () => {
-    assert.equal(transform('foo'), 'foo;');
-  })
+  it('transforms a self-closing JSX element', () => {
+    assert.equal(transform('<Foo />'), 'React.createElement(Foo )')
+  });
+
+  it('transforms nested JSX elements', () => {
+    assert.equal(transform('<div><span></span></div>'), 'React.createElement(Foo)')
+  });
+
+  it('transforms interpolated children', () => {
+    assert.equal(transform('<div>{x}</div>'), 'React.createElement(Foo)')
+  });
+
+  it('handles string property values', () => {
+    assert.equal(transform("<A foo='bar' />"), 'React.createElement(Foo)')
+  });
 });

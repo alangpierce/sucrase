5221466f252e8825b45d8c788b1abb82c28ab518
Alan Pierce <alangpierce@gmail.com>
2017-09-30 17:40:58 -0500

    Add basic working version of JSX transform

--- src/transform.ts
+++ src/transform.ts
@@ -1,37 +1,197 @@
 import {tokenize} from 'babylon';
 
 export default function transform(code: string): string {
-  let result = '';
-  const tokens = tokenize(code, {sourceType: 'module', plugins: ['jsx']});
+  let resultCode = '';
 
-  for (let i = 0; i < tokens.length; i++) {
-    const token = tokens[i];
-    result += code.slice(i > 0 ? tokens[i - 1].end : 0, token.start);
-    if (token.type.label === 'jsxTagStart' && tokens[i + 1].type.label === '/') {
-      // Closing tag.
-      result += ')';
-      while (tokens[i].type.label !== 'jsxTagEnd') {
-        i++;
+  let tokens = tokenize(code, {sourceType: 'module', plugins: ['jsx']});
+  tokens = tokens.filter((token) =>
+    token.type !== 'CommentLine' && token.type !== 'CommentBlock');
+  let tokenIndex = 0;
+
+  function matchesAtIndex(index: number, tagLabels: Array<string>): boolean {
+    for (let i = 0; i < tagLabels.length; i++) {
+      if (index + i >= tokens.length) {
+        return false;
       }
-    } else if (token.type.label === 'jsxTagStart') {
-      result += 'React.createElement(';
-      if (tokens[i + 1].type.label === 'jsxName' && startsWithLowerCase(tokens[i + 1].value)) {
-        i++;
-        result += `'${tokens[i].value}'`;
+      if (tokens[index + i].type.label !== tagLabels[i]) {
+        return false;
       }
-    } else if (token.type.label === 'jsxTagEnd') {
-      result += ', ';
-    } else if (token.type.label === '/' && tokens[i + 1].type.label === 'jsxTagEnd') {
-      result += ')';
-      i++;
-    } else {
-      result += code.slice(token.start, token.end);
+    }
+    return true;
+  }
+
+  function matches(tagLabels: Array<string>): boolean {
+    return matchesAtIndex(tokenIndex, tagLabels);
+  }
+
+  /**
+   * Produce the props arg to createElement, starting at the first token of the
+   * props, if any.
+   */
+  function processProps() {
+    if (!matches(['jsxName']) && !matches(['{'])) {
+      resultCode += ', null';
+      return;
+    }
+    resultCode += ', {';
+    while (true) {
+      if (matches(['jsxName', '='])) {
+        copyToken();
+        replaceToken(': ');
+        if (matches(['{'])) {
+          replaceToken('');
+          processBalancedCode();
+          replaceToken('');
+        } else {
+          copyToken();
+        }
+      } else if (matches(['jsxName'])) {
+        copyToken();
+        resultCode += ': true';
+      } else if (matches(['{'])) {
+        replaceToken('');
+        processBalancedCode();
+        replaceToken('');
+      } else {
+        break;
+      }
+      resultCode += ',';
+    }
+    resultCode += '}';
+  }
+
+  /**
+   * Process the first part of a tag, before any props.
+   */
+  function processTagIntro() {
+    // Walk forward until we see one of these patterns:
+    // [jsxIdentifer, equals] to start the first prop.
+    // [open brace] to start the first prop.
+    // [jsxTagEnd] to end the open-tag.
+    // [slash, jsxTagEnd] to end the self-closing tag.
+    let introEnd = tokenIndex + 1;
+    while (
+      !matchesAtIndex(introEnd, ['jsxName', '=']) &&
+      !matchesAtIndex(introEnd, ['{']) &&
+      !matchesAtIndex(introEnd, ['jsxTagEnd']) &&
+      !matchesAtIndex(introEnd, ['/', 'jsxTagEnd'])
+    ) {
+      introEnd++;
+    }
+    if (introEnd === tokenIndex + 1 && startsWithLowerCase(tokens[tokenIndex].value)) {
+      replaceToken(`'${tokens[tokenIndex].value}'`);
+    }
+    while (tokenIndex < introEnd) {
+      copyToken();
     }
   }
-  console.log(result);
-  return result;
+
+  function processChildren() {
+    while (true) {
+      if (matches(['jsxTagStart', '/'])) {
+        // Closing tag, so no more children.
+        return;
+      }
+      resultCode += ', ';
+      if (matches(['{'])) {
+        // Interpolated expression.
+        replaceToken('');
+        processBalancedCode();
+        replaceToken('');
+      } else if (matches(['jsxTagStart'])) {
+        // Child JSX element
+        processJSXTag();
+      } else if (matches(['jsxText'])) {
+        const value = tokens[tokenIndex].value;
+        resultCode += formatJSXTextLiteral(value);
+        // Emit blank lines to make sure the line numbers match up. Note this
+        // results in the last line losing all indentation, so it could be
+        // improved in the future.
+        replaceToken(value.split('\n').map((s: string) => '').join('\n'));
+      } else {
+        throw new Error('Unexpected token when processing JSX children.');
+      }
+    }
+  }
+
+  function processJSXTag() {
+    // First tag is always jsxTagStart.
+    replaceToken('React.createElement(');
+    processTagIntro();
+    processProps();
+
+    if (matches(['/', 'jsxTagEnd'])) {
+      // Self-closing tag.
+      replaceToken('');
+      replaceToken(')')
+    } else if (matches(['jsxTagEnd'])) {
+      replaceToken('');
+      // Tag with children.
+      processChildren();
+      while (!matches(['jsxTagEnd'])) {
+        replaceToken('');
+      }
+      replaceToken(')');
+    } else {
+      throw new Error('Expected either /> or > at the end of the tag.');
+    }
+  }
+
+  function processBalancedCode() {
+    let braceDepth = 0;
+    while (tokenIndex < tokens.length) {
+      if (matches(['jsxTagStart'])) {
+        processJSXTag();
+      } else {
+        if (matches(['{'])) {
+          braceDepth++;
+        } else if (matches(['}'])) {
+          if (braceDepth === 0) {
+            return;
+          }
+          braceDepth--;
+        }
+        copyToken();
+      }
+    }
+  }
+
+  function replaceToken(newCode: string) {
+    resultCode += code.slice(
+      tokenIndex > 0 ? tokens[tokenIndex - 1].end : 0, tokens[tokenIndex].start);
+    resultCode += newCode;
+    tokenIndex++;
+  }
+
+  function copyToken() {
+    resultCode += code.slice(
+      tokenIndex > 0 ? tokens[tokenIndex - 1].end : 0, tokens[tokenIndex].start);
+    resultCode += code.slice(tokens[tokenIndex].start, tokens[tokenIndex].end);
+    tokenIndex++;
+  }
+
+  processBalancedCode();
+  resultCode += code.slice(tokens[tokens.length - 1].end);
+  return resultCode;
 }
 
 function startsWithLowerCase(s: string): boolean {
   return s[0] == s[0].toLowerCase();
+}
+
+/**
+ * Turn the given jsxText string into a JS string literal.
+ *
+ * We use JSON.stringify to introduce escape characters as necessary, and trim
+ * the start and end of each line and remove blank lines.
+ */
+function formatJSXTextLiteral(text: string): string {
+  // Introduce fake characters at the start and end to avoid trimming the start
+  // of the first line or the end of the last line.
+  let lines = `!${text}!`.split('\n');
+  lines = lines.map((line) => line.trim());
+  lines[0] = lines[0].slice(1);
+  lines[lines.length - 1] = lines[lines.length - 1].slice(0, -1);
+  lines = lines.filter((line) => line);
+  return JSON.stringify(lines.join(' '));
 }
--- test/transform-test.ts
+++ test/transform-test.ts
@@ -2,20 +2,96 @@ import * as assert from 'assert';
 
 import transform from '../src/transform';
 
+function assertResult(code: string, expectedResult: string): void {
+  assert.equal(transform(code), expectedResult);
+}
+
 describe('transform', () => {
   it('transforms a self-closing JSX element', () => {
-    assert.equal(transform('<Foo />'), 'React.createElement(Foo )')
+    assertResult(`
+      <Foo />
+    `, `
+      React.createElement(Foo, null )
+    `);
   });
 
   it('transforms nested JSX elements', () => {
-    assert.equal(transform('<div><span></span></div>'), 'React.createElement(Foo)')
+    assertResult(`
+      <div><span></span></div>
+    `, `
+      React.createElement('div', null, React.createElement('span', null))
+    `);
   });
 
   it('transforms interpolated children', () => {
-    assert.equal(transform('<div>{x}</div>'), 'React.createElement(Foo)')
+    assertResult(`
+      <div>{x}</div>
+    `, `
+      React.createElement('div', null, x)
+    `);
   });
 
   it('handles string property values', () => {
-    assert.equal(transform("<A foo='bar' />"), 'React.createElement(Foo)')
+    assertResult(`
+      <A foo='bar' />
+    `, `
+      React.createElement(A, { foo: 'bar',} )
+    `);
+  });
+
+  it('handles inline comments', () => {
+    assertResult(`
+      <A
+        b='c' // A comment
+        d='e' /* Another comment */
+      />
+    `, `
+      React.createElement(A, {
+        b: 'c', // A comment
+        d: 'e',} /* Another comment */
+      )
+    `);
+  });
+
+  it('handles multiline strings', () => {
+    assertResult(`
+      const x = (
+        <div>
+          foo  bar
+          baz
+        </div>
+      );
+    `, `
+      const x = (
+        React.createElement('div', null, "foo  bar baz"
+
+
+)
+      );
+    `);
+  });
+
+  it('handles complex lower-case tag values', () => {
+    assertResult(`
+      <a.b c='d' />
+    `, `
+      React.createElement(a.b, { c: 'd',} )
+    `);
+  });
+
+  it('handles prop spread operators', () => {
+    assertResult(`
+      <a {...b} c='d' />
+    `, `
+      React.createElement('a', { ...b, c: 'd',} )
+    `);
+  });
+
+  it('handles HTML entities', () => {
+    assertResult(`
+      <span>a&nbsp;b</span>
+    `, `
+      React.createElement('span', null, "a b")
+    `);
   });
 });

d4136cd7d7fdbf1dd63d4de9d9bbd30ae2065d9d
Alan Pierce <alangpierce@gmail.com>
2017-09-30 19:17:34 -0500

    Add a real benchmark comparing with Babel

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,8 +1,23 @@
 #!/usr/bin/env node
 require('ts-node/register');
+const babel = require('babel-core');
 const fs = require('fs');
 const transform = require('../src/transform').default;
 
 const code = fs.readFileSync(process.argv[2]).toString();
-const newCode = transform(code);
-console.log(newCode);
+
+transform(code);
+transform(code);
+console.time('Hyperbole');
+for (let i = 0; i < 100; i++) {
+  transform(code);
+}
+console.timeEnd('Hyperbole');
+
+babel.transform(code, {presets: ['react']});
+babel.transform(code, {presets: ['react']});
+console.time('Babel');
+for (let i = 0; i < 100; i++) {
+  babel.transform(code, {presets: ['react']});
+}
+console.timeEnd('Babel');

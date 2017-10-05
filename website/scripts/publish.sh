dcf770bcae927f35198fdcd999f3bf9db4b5d7dc
Alan Pierce <alangpierce@gmail.com>
2017-10-05 08:14:17 -0700

    Fix publish script to copy all files to the top level

--- website/scripts/publish.sh
+++ website/scripts/publish.sh
@@ -7,7 +7,7 @@ yarn build
 rm -rf ./gh-pages
 git clone git@github.com:alangpierce/sucrase.git --branch gh-pages gh-pages
 rm -rf ./gh-pages/*
-cp -r build gh-pages
+cp -r build/* gh-pages
 cd gh-pages
 git add -A
 git commit -m 'Update website'

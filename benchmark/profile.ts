6a6b179650bf1c1d74a7fbde78112a1a4a64d193
Alan Pierce <alangpierce@gmail.com>
2017-12-20 11:06:43 -0800

    Add change babylon babel config to loose mode, add profile script

---
+++ benchmark/profile.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import * as fs from 'fs';
+
+import * as sucrase from '../src/index';
+
+function main(): void {
+  console.log('Profiling Sucrase on about 100,000 LOC. Make sure you have Chrome DevTools for Node open.');
+  const code = fs.readFileSync('./benchmark/sample.js').toString();
+  (console as any).profile('Sucrase');
+  for (let i = 0; i < 100; i++) {
+    sucrase.transform(
+      code,
+      {transforms: ['jsx', 'imports', 'add-module-exports', 'react-display-name']}
+    );
+  }
+  (console as any).profileEnd('Sucrase');
+}
+
+if (require.main === module) {
+  main();
+}

04c6d3aa0f4f7ce3ac22b41f2793847f4bc31c62
Alan Pierce <alangpierce@gmail.com>
2018-01-13 14:29:34 -0800

    Add a gulp plugin (#78)
    This is the first non-core package, so it's a first pass at a monorepo. The
    build and linting are all done at the top level and all are run in CI. I
    intentionally named the folder "integrations" instead of "packages", since my
    hope is that that will be the only use case for non-core packages. Hopefully,
    those integration packages can be small and rarely-published, unlike the many
    packages in the Babel monorepo.
    
    Maybe later I'll explore Lerna and/or a Yarn workspace, but for now I'm keeping
    it simple.

---
+++ integrations/gulp-plugin/src/index.ts
@@ -0,0 +1,42 @@
+import {Transform} from "stream";
+
+import {Options, transform} from "sucrase";
+
+import PluginError = require("plugin-error");
+import replaceExt = require("replace-ext");
+import through = require("through2");
+
+const PLUGIN_NAME = "@sucrase/gulp-plugin";
+
+function gulpSucrase(options: Options): Transform {
+  return through.obj(function(
+    this: Transform,
+    // tslint:disable-next-line no-any
+    file: any,
+    enc: string,
+    // tslint:disable-next-line no-any
+    cb: (err?: any, data?: any) => void,
+  ): void {
+    if (file.isNull()) {
+      cb(null, file);
+      return;
+    }
+    if (file.isStream()) {
+      cb(new PluginError(PLUGIN_NAME, "Streaming is not supported."));
+      return;
+    }
+
+    try {
+      const resultCode = transform(file.contents.toString(), options);
+      file.contents = Buffer.from(resultCode);
+      file.path = replaceExt(file.path, ".js");
+      this.push(file);
+    } catch (e) {
+      this.emit("error", new PluginError(PLUGIN_NAME, e));
+    }
+
+    cb();
+  });
+}
+
+module.exports = gulpSucrase;

c57e5ce430b041eea4809f1998fe73892d51759e
Alan Pierce <alangpierce@gmail.com>
2018-01-15 23:34:46 -0800

    Run all examples and the benchmark on CI
    This should make it safer to make parser changes, although it'll also likely
    slow down the build quite a bit. The benchmark is also nice since it gives a
    quick record of perf over time.
    
    Also add a jest plugin to get the apollo build to work properly.

---
+++ integrations/jest-plugin/src/index.ts
@@ -0,0 +1,21 @@
+import {Transform, transform} from "sucrase";
+
+function getTransforms(filename: string): Array<Transform> | null {
+  if (filename.endsWith(".js") || filename.endsWith(".jsx")) {
+    return ["flow", "jsx", "imports"];
+  } else if (filename.endsWith(".ts")) {
+    return ["typescript", "imports"];
+  } else if (filename.endsWith(".tsx")) {
+    return ["typescript", "jsx", "imports"];
+  }
+  return null;
+}
+
+export function process(src: string, filename: string): string {
+  const transforms = getTransforms(filename);
+  if (transforms !== null) {
+    return transform(src, {transforms});
+  } else {
+    return src;
+  }
+}

b99524efc1a2993dd60f1e7f10189bdb1267e7dd
Alan Pierce <alangpierce@gmail.com>
2018-01-15 18:24:12 -0800

    Add a simple option to exclude directories from the CLI (#108)
    Also hook it up to to the tslint build so that we don't attempt to compile test
    fixtures.

--- src/cli.ts
+++ src/cli.ts
@@ -13,6 +13,7 @@ export default function run(): void {
       "-d, --out-dir <out>",
       "Compile an input directory of modules into an output directory.",
     )
+    .option("--exclude-dirs <paths>", "Names of directories that should not be traversed.")
     .option("-t, --transforms <transforms>", "Comma-separated list of transforms to run.")
     .parse(process.argv);
 
@@ -34,8 +35,9 @@ export default function run(): void {
   const outDir = commander.outDir;
   const transforms = commander.transforms.split(",");
   const srcDir = commander.args[0];
+  const excludeDirs = commander.excludeDirs ? commander.excludeDirs.split(",") : [];
 
-  buildDirectory(srcDir, outDir, transforms).catch((e) => {
+  buildDirectory(srcDir, outDir, excludeDirs, transforms).catch((e) => {
     process.exitCode = 1;
     console.error(e);
   });
@@ -44,6 +46,7 @@ export default function run(): void {
 async function buildDirectory(
   srcDirPath: string,
   outDirPath: string,
+  excludeDirs: Array<string>,
   transforms: Array<Transform>,
 ): Promise<void> {
   const extension = transforms.includes("typescript") ? ".ts" : ".js";
@@ -51,13 +54,13 @@ async function buildDirectory(
     await mkdir(outDirPath);
   }
   for (const child of await readdir(srcDirPath)) {
-    if (["node_modules", ".git"].includes(child)) {
+    if (["node_modules", ".git"].includes(child) || excludeDirs.includes(child)) {
       continue;
     }
     const srcChildPath = join(srcDirPath, child);
     const outChildPath = join(outDirPath, child);
     if ((await stat(srcChildPath)).isDirectory()) {
-      await buildDirectory(srcChildPath, outChildPath, transforms);
+      await buildDirectory(srcChildPath, outChildPath, excludeDirs, transforms);
     } else if (srcChildPath.endsWith(extension)) {
       const outPath = `${outChildPath.substr(0, outChildPath.length - 3)}.js`;
       await buildFile(srcChildPath, outPath, transforms);

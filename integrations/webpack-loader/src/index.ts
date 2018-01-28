44590e52cad27a708dc9ffdd0e4a6ebd1f3ef033
Alan Pierce <alangpierce@gmail.com>
2018-01-28 14:08:05 -0800

    Update all integrations to pass the filePath param (#179)
    This should make error messages better, and is necessary for JSX debug info to
    work right.

--- integrations/webpack-loader/src/index.ts
+++ integrations/webpack-loader/src/index.ts
@@ -1,9 +1,11 @@
-import {getOptions} from "loader-utils";
+import {getOptions, getRemainingRequest} from "loader-utils";
 import {Options, transform} from "sucrase";
 
 function loader(code: string): string {
+  const webpackRemainingChain = getRemainingRequest(this).split("!");
+  const filePath = webpackRemainingChain[webpackRemainingChain.length - 1];
   const options: Options = getOptions(this) as Options;
-  return transform(code, options);
+  return transform(code, {filePath, ...options});
 }
 
 export = loader;

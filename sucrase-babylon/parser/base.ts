84da4916e86c43ecb09c29d13bcb90b10793b4f9
Alan Pierce <alangpierce@gmail.com>
2018-05-19 20:41:03 -0700

    Reimplement lines-and-columns, other AssemblyScript fixes

--- sucrase-babylon/parser/base.ts
+++ sucrase-babylon/parser/base.ts
@@ -1,6 +1,5 @@
-import LinesAndColumns from "lines-and-columns";
-
 import State from "../tokenizer/state";
+import {charCodes} from "../util/charcodes";
 
 export let isJSXEnabled: boolean;
 export let isTypeScriptEnabled: boolean;
@@ -19,16 +18,36 @@ export function getNextContextId(): number {
 // of the error message, and then raises a `SyntaxError` with that
 // message.
 export function raise(pos: number, message: string): never {
-  let loc = new LinesAndColumns(input).locationForIndex(pos);
-  if (!loc) {
-    loc = {line: 0, column: 0};
-  }
+  const loc = locationForIndex(pos);
   message += ` (${loc.line}:${loc.column})`;
   // tslint:disable-next-line no-any
   const err: any = new SyntaxError(message);
   err.pos = pos;
   err.loc = loc;
   throw err;
+}
+
+export class Loc {
+  line: number;
+  column: number;
+  constructor(line: number, column: number) {
+    this.line = line;
+    this.column = column;
+  }
+}
+
+export function locationForIndex(pos: number): Loc {
+  let line = 1;
+  let column = 1;
+  for (let i = 0; i < pos; i++) {
+    if (input.charCodeAt(pos) === charCodes.lineFeed) {
+      line++;
+      column = 1;
+    } else {
+      column++;
+    }
+  }
+  return new Loc(line, column);
 }
 
 export function initParser(

75e946b2a014e4f1232e460a80adc8d8c2359965
Alan Pierce <alangpierce@gmail.com>
2018-08-27 07:44:13 -0700

    Update release script to use npm publish instead of yarn publish
    Still waiting on yarn to support 2FA: https://github.com/yarnpkg/yarn/issues/4904 ,
    so this change switches to npm in the meantime.

--- script/release.ts
+++ script/release.ts
@@ -5,21 +5,20 @@ import sleep from "./sleep";
 
 /**
  * To use:
- * 1.) Add changelog notes for the new version without committing.
- * 2.) Run this script with the new version number, e.g. "yarn release 1.2.3".
- * 3.) Verify that everything in the commit looks right and push.
+ * 1.) Update package.json to the new version without committing.
+ * 2.) Add changelog notes for the new version without committing.
+ * 3.) Run this script with "yarn release" from the root directory.
+ * 4.) Verify that everything in the commit looks right and push.
  */
 async function main(): Promise<void> {
-  const version = process.argv[2];
-  if (version.split(".").length !== 3 || version.includes(" ")) {
-    throw new Error(`Invalid version: ${version}`);
-  }
+  const packageJSON = JSON.parse((await readFile("./package.json")).toString());
+  const version = packageJSON.version;
   const changelogText = await readFile("./CHANGELOG.md");
   if (!changelogText.includes(version)) {
     throw new Error("Release notes must be already added to changelog.");
   }
-  console.log("Version is valid and found in changelog.");
-  await run(`yarn publish --new-version ${version}`);
+  console.log(`Version ${version} is valid and found in changelog.`);
+  await run("npm publish");
   console.log("Taking a quick nap to make sure we update with the right version.");
   await sleep(15000);
   await run("yarn add sucrase@latest");
@@ -27,7 +26,7 @@ async function main(): Promise<void> {
   await run("yarn add sucrase@latest");
   await run("yarn publish-website");
   process.chdir("..");
-  await run("git commit -a --amend --no-edit");
+  await run(`git commit -a -m "v${version}"`);
   console.log("Done! Please sanity-check the commit, then push.");
 }
 

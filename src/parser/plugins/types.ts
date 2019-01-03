8359a899fe829756924c4c6aad1a36f0d547cb88
Alan Pierce <alangpierce@gmail.com>
2019-01-02 21:32:31 -0800

    Properly handle optional arrow params without type parameters
    Fixes #387
    
    I reworked `typedParseConditional` significantly from the Babel implementation
    and didn't realize that I needed to preserve bare `?` tokens to denote the
    parameter optional. Fortunately, the optional case will always be `?,` or `?)`
    (an initializer isn't allowed when the param is optional), and neither of those
    cases is valid as the start of a ternary, so we can distinguish optional param
    from ternary with only one token of lookahead.

--- src/parser/plugins/types.ts
+++ src/parser/plugins/types.ts
@@ -12,9 +12,13 @@ import {tsParseTypeAnnotation} from "./typescript";
 // An apparent conditional expression could actually be an optional parameter in an arrow function.
 export function typedParseConditional(noIn: boolean): void {
   // If we see ?:, this can't possibly be a valid conditional. typedParseParenItem will be called
-  // later to finish off the arrow parameter.
-  if (match(tt.question) && lookaheadType() === tt.colon) {
-    return;
+  // later to finish off the arrow parameter. We also need to handle bare ? tokens for optional
+  // parameters without type annotations, i.e. ?, and ?) .
+  if (match(tt.question)) {
+    const nextType = lookaheadType();
+    if (nextType === tt.colon || nextType === tt.comma || nextType === tt.parenR) {
+      return;
+    }
   }
   baseParseConditional(noIn);
 }

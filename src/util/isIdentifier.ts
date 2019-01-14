46fc02bc201f06207393ebed8837ac37c97f16a9
Alan Pierce <alan@benchling.com>
2019-01-13 23:04:49 -0800

    Treat reserved words as invalid identifiers when handling enums (#405)
    Fixes #392
    
    There was already a code path for keys that couldn't be treated as regular
    identifiers; with those, the variable declaration is omitted and we create a
    special expression if necessary to add 1 to the previous value. Now, even for
    regular identifiers, we see if the resulting identifier would be a JS reserve
    word and treat it as a non-identifier if so. I had a few ideas on how to
    reliably get a list of reserved words, but decided to just reference MDN since
    there were enough edge cases with trying to modify the parser code.

--- src/util/isIdentifier.ts
+++ src/util/isIdentifier.ts
@@ -1,5 +1,59 @@
 import {IS_IDENTIFIER_CHAR, IS_IDENTIFIER_START} from "../parser/util/identifier";
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar
+// Hard-code a list of reserved words rather than trying to use keywords or contextual keywords
+// from the parser, since currently there are various exceptions, like `package` being reserved
+// but unused and various contextual keywords being reserved. Note that we assume that all code
+// compiled by Sucrase is in a module, so strict mode words and await are all considered reserved
+// here.
+const RESERVED_WORDS = new Set([
+  // Reserved keywords as of ECMAScript 2015
+  "break",
+  "case",
+  "catch",
+  "class",
+  "const",
+  "continue",
+  "debugger",
+  "default",
+  "delete",
+  "do",
+  "else",
+  "export",
+  "extends",
+  "finally",
+  "for",
+  "function",
+  "if",
+  "import",
+  "in",
+  "instanceof",
+  "new",
+  "return",
+  "super",
+  "switch",
+  "this",
+  "throw",
+  "try",
+  "typeof",
+  "var",
+  "void",
+  "while",
+  "with",
+  "yield",
+  // Future reserved keywords
+  "enum",
+  "implements",
+  "interface",
+  "let",
+  "package",
+  "private",
+  "protected",
+  "public",
+  "static",
+  "await",
+]);
+
 export default function isIdentifier(name: string): boolean {
   if (name.length === 0) {
     return false;
@@ -12,5 +66,5 @@ export default function isIdentifier(name: string): boolean {
       return false;
     }
   }
-  return true;
+  return !RESERVED_WORDS.has(name);
 }

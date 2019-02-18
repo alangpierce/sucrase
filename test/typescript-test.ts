cecfdeeceb6a73858e2b0517d193cba00a246304
Alan Pierce <alan@benchling.com>
2019-02-17 23:23:08 -0800

    Move star export code into a more official helper (#417)
    I'll need to change the implementation of this soon, so this should make it
    easier to modify.

--- test/typescript-test.ts
+++ test/typescript-test.ts
@@ -1,4 +1,5 @@
 import {
+  CREATE_STAR_EXPORT_PREFIX,
   ESMODULE_PREFIX,
   IMPORT_DEFAULT_PREFIX,
   IMPORT_WILDCARD_PREFIX,
@@ -834,8 +835,8 @@ describe("typescript transform", () => {
       `
       export * from './MyVars';
     `,
-      `"use strict";${ESMODULE_PREFIX}
-      var _MyVars = require('./MyVars'); Object.keys(_MyVars).filter(key => key !== 'default' && key !== '__esModule').forEach(key => { if (exports.hasOwnProperty(key)) { return; } Object.defineProperty(exports, key, {enumerable: true, get: () => _MyVars[key]}); });
+      `"use strict";${CREATE_STAR_EXPORT_PREFIX}${ESMODULE_PREFIX}
+      var _MyVars = require('./MyVars'); _createStarExport(_MyVars);
     `,
     );
   });

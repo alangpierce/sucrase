c2267eaf46c52e7641e59dcaa10245b6c81b047a
Raynos <raynos2@gmail.com>
2020-02-16 15:09:48 +0100

    src/cli: Add support for --project
    Here we can run `sucrase` and ask it to just read our tsconfig
    for information instead passing it N CLI arguments.
    
    It will read meta data from `tsconfig.json` which includes
    `files` & `include`.
    
    This allows it to just compile our project.
    
    Also in case you have an `include` with multiple folders like
    `src/**/*.ts` and `test/**/*.ts` then you can compile your
    project in one step.
    
    Currently `sucrase` only supports one entry dir instead of
    multiple entry dirs.

--- src/cli.ts
+++ src/cli.ts
@@ -1,16 +1,23 @@
 /* eslint-disable no-console */
 import commander from "commander";
+import globCb from "glob";
 import {exists, mkdir, readdir, readFile, stat, writeFile} from "mz/fs";
-import {join} from "path";
+import {join, relative, dirname} from "path";
+import {promisify} from "util";
 
 import {Options, transform} from "./index";
 
 interface CLIOptions {
+  outDirPath: string;
+  srcDirPath: string;
+  project: string;
   outExtension: string;
   excludeDirs: Array<string>;
   quiet: boolean;
   sucraseOptions: Options;
 }
+
+const glob = promisify(globCb);
 
 export default function run(): void {
   commander
@@ -19,6 +26,10 @@ export default function run(): void {
     .option(
       "-d, --out-dir <out>",
       "Compile an input directory of modules into an output directory.",
+    )
+    .option(
+      "-p, --project <dir>",
+      "Compile a typescript project, will read from tsconfig.json in <dir>",
     )
     .option("--out-extension <extension>", "File extension to use for all output files.", "js")
     .option("--exclude-dirs <paths>", "Names of directories that should not be traversed.")
@@ -33,30 +44,30 @@ export default function run(): void {
     .option("--jsx-fragment-pragma <string>", "Fragment component, defaults to `React.Fragment`")
     .parse(process.argv);
 
-  if (!commander.outDir) {
+  if (!commander.outDir && !commander.project) {
     console.error("Out directory is required");
     process.exit(1);
   }
 
-  if (!commander.transforms) {
+  if (!commander.transforms && !commander.project) {
     console.error("Transforms option is required.");
     process.exit(1);
   }
 
-  if (!commander.args[0]) {
+  if (!commander.args[0] && !commander.project) {
     console.error("Source directory is required.");
     process.exit(1);
   }
 
-  const outDir = commander.outDir;
-  const srcDir = commander.args[0];
-
   const options: CLIOptions = {
+    outDirPath: commander.outDir,
+    srcDirPath: commander.args[0],
+    project: commander.project,
     outExtension: commander.outExtension,
     excludeDirs: commander.excludeDirs ? commander.excludeDirs.split(",") : [],
     quiet: commander.quiet,
     sucraseOptions: {
-      transforms: commander.transforms.split(","),
+      transforms: commander.transforms ? commander.transforms.split(",") : [],
       enableLegacyTypeScriptModuleInterop: commander.enableLegacyTypescriptModuleInterop,
       enableLegacyBabel5ModuleInterop: commander.enableLegacyBabel5ModuleInterop,
       jsxPragma: commander.jsxPragma || "React.createElement",
@@ -64,23 +75,30 @@ export default function run(): void {
     },
   };
 
-  buildDirectory(srcDir, outDir, options).catch((e) => {
+  buildDirectory(options).catch((e) => {
     process.exitCode = 1;
     console.error(e);
   });
 }
 
-async function buildDirectory(
-  srcDirPath: string,
-  outDirPath: string,
-  options: CLIOptions,
-): Promise<void> {
+interface FileInfo {
+  srcPath: string;
+  outPath: string;
+}
+
+async function findFiles(options: CLIOptions): Promise<Array<FileInfo>> {
+  const outDirPath = options.outDirPath;
+  const srcDirPath = options.srcDirPath;
+
   const extensions = options.sucraseOptions.transforms.includes("typescript")
     ? [".ts", ".tsx"]
     : [".js", ".jsx"];
+
   if (!(await exists(outDirPath))) {
     await mkdir(outDirPath);
   }
+
+  const outArr: Array<FileInfo> = [];
   for (const child of await readdir(srcDirPath)) {
     if (["node_modules", ".git"].includes(child) || options.excludeDirs.includes(child)) {
       continue;
@@ -88,11 +106,166 @@ async function buildDirectory(
     const srcChildPath = join(srcDirPath, child);
     const outChildPath = join(outDirPath, child);
     if ((await stat(srcChildPath)).isDirectory()) {
-      await buildDirectory(srcChildPath, outChildPath, options);
+      const innerOptions = {...options};
+      innerOptions.srcDirPath = srcChildPath;
+      innerOptions.outDirPath = outChildPath;
+      const innerFiles = await findFiles(innerOptions);
+      outArr.push(...innerFiles);
     } else if (extensions.some((ext) => srcChildPath.endsWith(ext))) {
       const outPath = outChildPath.replace(/\.\w+$/, `.${options.outExtension}`);
-      await buildFile(srcChildPath, outPath, options);
+      outArr.push({
+        srcPath: srcChildPath,
+        outPath,
+      });
     }
+  }
+
+  return outArr;
+}
+
+async function runGlob(options: CLIOptions): Promise<Array<FileInfo>> {
+  const tsConfigPath = join(options.project, "tsconfig.json");
+
+  let str;
+  try {
+    str = await readFile(tsConfigPath, "utf8");
+  } catch (err) {
+    console.error("Could not find project tsconfig.json");
+    console.error(`  --project=${options.project}`);
+    console.error(err);
+    process.exit(1);
+  }
+  const json = JSON.parse(str);
+
+  const foundFiles: Array<FileInfo> = [];
+
+  const files = json.files;
+  const include = json.include;
+
+  const absProject = join(process.cwd(), options.project);
+  const outDirs: Array<string> = [];
+
+  if (!(await exists(options.outDirPath))) {
+    await mkdir(options.outDirPath);
+  }
+
+  if (files) {
+    for (const file of files) {
+      if (file.endsWith(".d.ts")) {
+        continue;
+      }
+      if (!file.endsWith(".ts") && !file.endsWith(".js")) {
+        continue;
+      }
+
+      const srcFile = join(absProject, file);
+      const outFile = join(options.outDirPath, file);
+      const outPath = outFile.replace(/\.\w+$/, `.${options.outExtension}`);
+
+      const outDir = dirname(outPath);
+      if (!outDirs.includes(outDir)) {
+        outDirs.push(outDir);
+      }
+
+      foundFiles.push({
+        srcPath: srcFile,
+        outPath,
+      });
+    }
+  }
+  if (include) {
+    for (const pattern of include) {
+      const globFiles = await glob(join(absProject, pattern));
+      for (const file of globFiles) {
+        if (!file.endsWith(".ts") && !file.endsWith(".js")) {
+          continue;
+        }
+        if (file.endsWith(".d.ts")) {
+          continue;
+        }
+
+        const relativeFile = relative(absProject, file);
+        const outFile = join(options.outDirPath, relativeFile);
+        const outPath = outFile.replace(/\.\w+$/, `.${options.outExtension}`);
+
+        const outDir = dirname(outPath);
+        if (!outDirs.includes(outDir)) {
+          outDirs.push(outDir);
+        }
+
+        foundFiles.push({
+          srcPath: file,
+          outPath,
+        });
+      }
+    }
+  }
+
+  for (const outDirPath of outDirs) {
+    if (!(await exists(outDirPath))) {
+      await mkdir(outDirPath);
+    }
+  }
+
+  // TODO: read exclude
+
+  return foundFiles;
+}
+
+async function updateOptionsFromProject(options: CLIOptions): Promise<void> {
+  /**
+   * Read the project information and assign the following.
+   *  - outDirPath
+   *  - transform: imports
+   *  - transform: typescript
+   *  - enableLegacyTypescriptModuleInterop: true/false.
+   */
+
+  const tsConfigPath = join(options.project, "tsconfig.json");
+
+  let str;
+  try {
+    str = await readFile(tsConfigPath, "utf8");
+  } catch (err) {
+    console.error("Could not find project tsconfig.json");
+    console.error(`  --project=${options.project}`);
+    console.error(err);
+    process.exit(1);
+  }
+  const json = JSON.parse(str);
+  const sucraseOpts = options.sucraseOptions;
+  if (!sucraseOpts.transforms.includes("typescript")) {
+    sucraseOpts.transforms.push("typescript");
+  }
+
+  const compilerOpts = json.compilerOptions;
+  if (compilerOpts.outDir) {
+    options.outDirPath = join(process.cwd(), options.project, compilerOpts.outDir);
+  }
+  if (compilerOpts.esModuleInterop !== true) {
+    sucraseOpts.enableLegacyTypeScriptModuleInterop = true;
+  }
+  if (compilerOpts.module === "commonjs") {
+    if (!sucraseOpts.transforms.includes("imports")) {
+      sucraseOpts.transforms.push("imports");
+    }
+  }
+}
+
+async function buildDirectory(options: CLIOptions): Promise<void> {
+  let files: Array<FileInfo>;
+  if (options.outDirPath && options.srcDirPath) {
+    files = await findFiles(options);
+  } else if (options.project) {
+    await updateOptionsFromProject(options);
+    files = await runGlob(options);
+  } else {
+    console.error("Project or Source directory required.");
+    process.exit(1);
+  }
+
+  for (const file of files) {
+    await buildFile(file.srcPath, file.outPath, options);
   }
 }
 

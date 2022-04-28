5609ba8c29e45ec20dd9a81bd22a7b4080a69362
Viktor Luft <viktor.luft@freiheit.com>
2022-04-28 09:21:09 +0200

    Jest-Plugin: Support v28

--- integrations/jest-plugin/src/index.ts
+++ integrations/jest-plugin/src/index.ts
@@ -11,7 +11,11 @@ function getTransforms(filename: string): Array<Transform> | null {
   return null;
 }
 
-export function process(src: string, filename: string): string {
+// this is compatible to the one that is required by Jest, using the type from here:
+// https://github.com/mozilla/source-map/blob/0.6.1/source-map.d.ts#L6-L12
+type RawSourceMap = ReturnType<typeof transform>['sourceMap'];
+
+export function process(src: string, filename: string): {code: string; map?: RawSourceMap | string | null} {
   const transforms = getTransforms(filename);
   if (transforms !== null) {
     const {code, sourceMap} = transform(src, {
@@ -21,9 +25,9 @@ export function process(src: string, filename: string): string {
     });
     const mapBase64 = Buffer.from(JSON.stringify(sourceMap)).toString("base64");
     const suffix = `//# sourceMappingURL=data:application/json;charset=utf-8;base64,${mapBase64}`;
-    return `${code}\n${suffix}`;
+    return {code: `${code}\n${suffix}`, map: sourceMap};
   } else {
-    return src;
+    return {code: src};
   }
 }
 

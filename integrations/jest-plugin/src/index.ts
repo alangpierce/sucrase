53105e0aaa90a5e700ba4c650d5717f23a9f135d
Viktor Luft <viktor.luft@freiheit.com>
2022-04-28 10:16:51 +0200

    Fix prettier issues

--- integrations/jest-plugin/src/index.ts
+++ integrations/jest-plugin/src/index.ts
@@ -13,9 +13,12 @@ function getTransforms(filename: string): Array<Transform> | null {
 
 // this is compatible to the one that is required by Jest, using the type from here:
 // https://github.com/mozilla/source-map/blob/0.6.1/source-map.d.ts#L6-L12
-type RawSourceMap = ReturnType<typeof transform>['sourceMap'];
+type RawSourceMap = ReturnType<typeof transform>["sourceMap"];
 
-export function process(src: string, filename: string): {code: string; map?: RawSourceMap | string | null} {
+export function process(
+  src: string,
+  filename: string,
+): {code: string; map?: RawSourceMap | string | null} {
   const transforms = getTransforms(filename);
   if (transforms !== null) {
     const {code, sourceMap} = transform(src, {

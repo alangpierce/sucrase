dc899cf645da921fd69e83a3248e4c400610aec8
cpitt <cpitt@firstam.com>
2022-06-03 11:41:48 -0700

    Add configurable: true to Object.defineProperty to allow overwriting duplicate exports.
    Consider the following valid code
    
    ```typescript
    // lib1.ts
    export const a = '1';
    
    // lib2.ts
    export const a = '2'
    
    // index.ts
    export * from 'lib1.ts'
    export * from 'lib2.ts'
    export { a } from 'lib2.ts'
    
    ```
    Currently tsc and babel compile this fine but sucrase fails with
    
    ```
    TypeError: Cannot redefine property: a
            at Function.defineProperty (<anonymous>)
    ```

--- test/prefixes.ts
+++ test/prefixes.ts
@@ -8,11 +8,11 @@ if (Object.prototype.hasOwnProperty.call(obj, key)) { newObj[key] = obj[key]; } 
 newObj.default = obj; return newObj; } }`;
 export const CREATE_NAMED_EXPORT_FROM_PREFIX = ` function _createNamedExportFrom(obj, \
 localName, importedName) { Object.defineProperty(exports, localName, \
-{enumerable: true, get: () => obj[importedName]}); }`;
+{enumerable: true, configurable: true, get: () => obj[importedName]}); }`;
 export const CREATE_STAR_EXPORT_PREFIX = ` function _createStarExport(obj) { \
 Object.keys(obj) .filter((key) => key !== "default" && key !== "__esModule") \
 .forEach((key) => { if (exports.hasOwnProperty(key)) { return; } \
-Object.defineProperty(exports, key, {enumerable: true, get: () => obj[key]}); }); }`;
+Object.defineProperty(exports, key, {enumerable: true, configurable: true, get: () => obj[key]}); }); }`;
 export const ESMODULE_PREFIX = 'Object.defineProperty(exports, "__esModule", {value: true});';
 export const RHL_PREFIX = `(function () { \
 var enterModule = require('react-hot-loader').enterModule; enterModule && enterModule(module); \

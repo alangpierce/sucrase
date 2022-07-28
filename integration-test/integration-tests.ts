a1df95f20b6fd8e0e2d39dc2326572ef3959b0a2
Alan Pierce <alangpierce@gmail.com>
2022-07-28 13:06:53 -0700

    Add more inline docs for the plugin and the test runner

--- integration-test/integration-tests.ts
+++ integration-test/integration-tests.ts
@@ -5,6 +5,13 @@ import {promisify} from "util";
 const execPromise = promisify(exec);
 
 describe("ts-node tests", async () => {
+  /**
+   * Find all integration tests in the test-cases directory.
+   *
+   * Each test has a file starting with "main" (e.g. main.ts, main.tsx,
+   * main.mts, etc) that's used as the entry point. The test should be written
+   * in such a way that the execution throws an exception if the test fails.
+   */
   async function* discoverTests(dir: string): AsyncIterable<string> {
     for (const child of await readdir(dir)) {
       const childPath = join(dir, child);
@@ -18,6 +25,9 @@ describe("ts-node tests", async () => {
 
   for await (const testPath of discoverTests("test-cases")) {
     it(testPath, async () => {
+      // To help confirm that the behavior is in sync with the default ts-node
+      // behavior, first run ts-node without the plugin to make sure it works,
+      // then run it with the plugin.
       await execPromise(`npx ts-node --esm --transpile-only ${testPath}`);
       await execPromise(
         `npx ts-node --esm --transpiler ${__dirname}/../ts-node-plugin ${testPath}`,
--- ts-node-plugin/index.js
+++ ts-node-plugin/index.js
@@ -1,26 +1,56 @@
 const {transform} = require("../dist");
 
+// Enum constants taken from the TypeScript codebase.
 const ModuleKindCommonJS = 1;
+
+const JsxEmitReactJSX = 4;
+const JsxEmitReactJSXDev = 5;
 
 /**
  * ts-node transpiler plugin
+ *
+ * This plugin hooks into ts-node so that Sucrase can handle all TS-to-JS
+ * conversion while ts-node handles the ESM loader, require hook, REPL
+ * integration, etc. ts-node automatically discovers the relevant tsconfig file,
+ * so the main logic in this integration is translating tsconfig options to the
+ * corresponding Sucrase options.
+ *
+ * Any tsconfig options relevant to Sucrase are translated, but some config
+ * options outside the scope of Sucrase are ignored. For example, we assume the
+ * isolatedModules option, and we ignore target because Sucrase doesn't provide
+ * JS syntax downleveling (at least not in a way that is useful for Node).
+ *
+ * One notable caveat is that importsNotUsedAsValues and preserveValueImports
+ * are ignored right now, and Sucrase uses TypeScript's default behavior of
+ * eliding imports only used as types. This usually makes no difference when
+ * running the code, so for now we ignore these options without a warning.
  */
 function create(createOptions) {
   const {nodeModuleEmitKind} = createOptions;
   const {module, jsx, jsxFactory, jsxFragmentFactory, esModuleInterop} =
     createOptions.service.config.options;
 
+  // A project with the new JSX transform configured likely has at least one
+  // file with JSX where React is not imported, so fail fast and suggest the
+  // old JSX transform as a workaround.
+  if (jsx === JsxEmitReactJSX || jsx === JsxEmitReactJSXDev) {
+    throw new Error(
+      'The JSX modes "react-jsx" and "react-jsxdev" are not yet ' +
+        'supported by Sucrase. Consider using "react" as a workaround.',
+    );
+  }
+
   return {
     transpile(input, transpileOptions) {
       const {fileName} = transpileOptions;
       const transforms = [];
+      // Detect JS rather than TS so we bias toward including the typescript
+      // transform, since almost always it doesn't hurt to include.
       const isJS =
         fileName.endsWith(".js") ||
         fileName.endsWith(".jsx") ||
         fileName.endsWith(".mjs") ||
         fileName.endsWith(".cjs");
-      // Detect JS rather than TS so we bias toward including the typescript
-      // transform, since almost always it doesn't hurt to include.
       if (!isJS) {
         transforms.push("typescript");
       }

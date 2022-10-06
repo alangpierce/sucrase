6357f56d508c7fe03d0b68135f2c32cca15a6134
Alan Pierce <alangpierce@gmail.com>
2022-10-06 00:18:03 -0700

    Add integration tests for jest plugin
    This PR reworks and extends the integration test suite to have two tests for the
    Jest plugin, and hopefully is flexible enough for additional cases in the
    future. Like the ts-node test suite, it relies on test cases that are defined by
    directories.

--- integration-test/integration-tests.ts
+++ integration-test/integration-tests.ts
@@ -1,42 +1,75 @@
 import {join} from "path";
-import {readdir, stat} from "fs/promises";
 import {exec} from "child_process";
 import {promisify} from "util";
+import {readdirSync, statSync} from "fs";
 const execPromise = promisify(exec);
+import {dirname} from "path";
+import {readJSONFileContentsIfExists} from "../script/util/readFileContents";
+import assert from "assert";
 
-describe("ts-node tests", async () => {
+describe("integration tests", () => {
+  const originalCwd = process.cwd();
+  afterEach(() => {
+    process.chdir(originalCwd);
+  });
+
   /**
-   * Find all integration tests in the test-cases directory.
-   *
-   * Each test has a file starting with "main" (e.g. main.ts, main.tsx,
-   * main.mts, etc) that's used as the entry point. The test should be written
-   * in such a way that the execution throws an exception if the test fails.
+   * Traverse the given directory for all subdirectories containing any file
+   * matching a prefix. We use sync methods here to avoid complexity around
+   * Mocha's --delay option.
    */
-  async function* discoverTests(dir: string): AsyncIterable<string> {
-    for (const child of await readdir(dir)) {
+  function* discoverTests(dir: string, filePrefix: string): Iterable<string> {
+    for (const child of readdirSync(dir)) {
       const childPath = join(dir, child);
-      if ((await stat(childPath)).isDirectory()) {
-        yield* discoverTests(childPath);
-      } else if (child.startsWith("main")) {
+      if (statSync(childPath).isDirectory()) {
+        yield* discoverTests(childPath, filePrefix);
+      } else if (child.startsWith(filePrefix)) {
         yield childPath;
       }
     }
   }
 
-  for await (const testPath of discoverTests("test-cases")) {
-    it(testPath, async () => {
-      // To help confirm that the behavior is in sync with the default ts-node
-      // behavior, first run ts-node without the plugin to make sure it works,
-      // then run it with the plugin.
-      await execPromise(`npx ts-node --esm --transpile-only ${testPath}`);
-      await execPromise(
-        `npx ts-node --esm --transpiler ${__dirname}/../ts-node-plugin ${testPath}`,
-      );
+  /**
+   * Find Jest integration tests.
+   *
+   * Each test directory has a jest.config.js and a test that should pass when
+   * run.
+   */
+  for (const testFile of discoverTests("test-cases/jest-cases", "jest.config.js")) {
+    const testDir = dirname(testFile);
+    it(testDir, async () => {
+      process.chdir(testDir);
+      const testConfig = await readJSONFileContentsIfExists("./test.json");
+      if (testConfig?.expectedError) {
+        try {
+          await execPromise(`npx jest`);
+          assert.fail("Expected Jest to fail");
+        } catch (e) {
+          assert((e as {stderr: string}).stderr.includes(testConfig.expectedError));
+        }
+      } else {
+        // Should not crash.
+        await execPromise(`npx jest`);
+      }
     });
   }
 
-  // Currently, mocha needs to be run with --delay to allow async test
-  // generation like this, and that also requires explicitly invoking this run
-  // callback.
-  run();
+  /**
+   * Find ts-node integration tests.
+   *
+   * Each test has a file starting with "main" (e.g. main.ts, main.tsx,
+   * main.mts, etc) that's used as the entry point. The test should be written
+   * in such a way that the execution throws an exception if the test fails.
+   */
+  for (const testFile of discoverTests("test-cases/ts-node-cases", "main")) {
+    it(testFile, async () => {
+      // To help confirm that the behavior is in sync with the default ts-node
+      // behavior, first run ts-node without the plugin to make sure it works,
+      // then run it with the plugin.
+      await execPromise(`npx ts-node --esm --transpile-only ${testFile}`);
+      await execPromise(
+        `npx ts-node --esm --transpiler ${__dirname}/../ts-node-plugin ${testFile}`,
+      );
+    });
+  }
 });
